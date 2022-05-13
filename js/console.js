4b2c7aa85eb4f5767490772c1a5b575d13c7fe00
Raj <chanchalraj.maity@gmail.com>
2022-05-13 22:20:26 +0530

    console.js modified

--- js/console.js
+++ js/console.js
@@ -1,2 +1,5 @@
 console.log("Hello World");
 console.table({name: "Raj", language: "JavaScript", number: 91});
+console.warn("This is a warning");
+console.assert(0>1,"expression is false");
+console.error("This is an error");
