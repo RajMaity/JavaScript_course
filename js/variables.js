89822cae9edf98a7e264996aaa6c671a1fcbedb0
Raj <chanchalraj.maity@gmail.com>
2022-05-14 21:25:41 +0530

    variable.js modified

--- js/variables.js
+++ js/variables.js
@@ -1,9 +1,18 @@
-let a=1;
-console.log(a);
-let global_var="This is global variable";
+// let a=1;
+// console.log(a);
+// let global_var="This is global variable";
+// function fun(){
+//     let local_variable="This is local variable";
+// }
+// fun();
+// console.log(global_var);
+// console.log(local_variable);
+
+
+var global_var="This is a global variable";
 function fun(){
-    let local_variable="This is local variable";
+    var local_var="This is a local variable";
+    console.log(local_var);
+    console.log(global_var);
 }
 fun();
-console.log(global_var);
-console.log(local_variable);
