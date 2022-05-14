0eef3e48ed60fb07350ab395e609b7e8dfed2bee
Raj <chanchalraj.maity@gmail.com>
2022-05-14 23:17:53 +0530

    datatype.js added

---
+++ js/Datatypes.js
@@ -0,0 +1,16 @@
+let str="Hello World"; //String
+let number=13;  //Number
+let istrue=true; //Boolean
+let isfalse=false; //Boolean
+let num=Infinity;  //infinity
+let new_num=NaN;
+var age=null; //null
+var x; //undefined
+console.log(str);
+console.log(number);
+console.log(istrue);
+console.log(isfalse);
+console.log(num);
+console.log(new_num);
+console.log(age);
+console.log(x);
