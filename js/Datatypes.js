b491880c96dfed9806a82a300d7ce84ea3ba0eba
Raj <chanchalraj.maity@gmail.com>
2022-05-15 00:54:06 +0530

    datatypes.js modified

--- js/Datatypes.js
+++ js/Datatypes.js
@@ -4,6 +4,7 @@ let istrue=true; //Boolean
 let isfalse=false; //Boolean
 let num=Infinity;  //infinity
 let new_num=NaN;
+let num1='kuch likha hai idhr'/2;
 var age=null; //null
 var x; //undefined
 console.log(str);
@@ -14,3 +15,9 @@ console.log(num);
 console.log(new_num);
 console.log(age);
 console.log(x);
+console.log(num1);
+  console.log('("3" - "1") = ' + ("3" - "1"));
+  console.log('("3" - 1) = ' + ("3" - 1));
+  console.log('("3" * "2") = ' + ("3" * "2"));
+  console.log('("3" % "2") = ' + ("3" % "2"));
+  console.log('("3" + null) = ' + ("3" + null));         
