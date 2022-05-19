7ec8b0153b142be46e6dac1e648082fc00f85725
Raj <chanchalraj.maity@gmail.com>
2022-05-20 00:43:42 +0530

    Operator.html and js added

---
+++ js/Operators.js
@@ -0,0 +1,20 @@
+//addition
+let a=2,b=3;
+console.log(a+b);
+
+//subtraction
+console.log(b-a);
+
+//multiplication
+console.log(a*b);
+
+//division
+console.log(b/a);
+
+//modulus
+console.log(b%a);
+
+//typeof
+let str="GeeksforGeeks";
+console.log(typeof(str));
+
