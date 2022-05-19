7ccc6ad5b03e700442c4b77649687ccccbc24cee
Raj <chanchalraj.maity@gmail.com>
2022-05-20 00:46:16 +0530

    Operators.js modified

--- js/Operators.js
+++ js/Operators.js
@@ -18,3 +18,23 @@ console.log(b%a);
 let str="GeeksforGeeks";
 console.log(typeof(str));
 
+var x = 6;
+var y = 1;
+// AND Operation
+console.log("A & B = " + (x & y));
+// OR operation
+console.log("A | B = " + (x | y));
+// NOT operation
+console.log("~A = " + (~x));
+// Sign Propagating Right Shift
+console.log("A >> B = " + (x >> y));
+// Zero Fill Right Shift
+console.log("A >>> B = " + (x >>> y));
+// Left Shift
+console.log("A << B = " + (x << y));  
+
+// ||(OR) Operator
+console.log( 0 || 1 ); // 1
+console.log( 1 || 3 ); // 1
+console.log( null || true ); // true
+console.log( -1 || -2 || -3 || -4); // -1
