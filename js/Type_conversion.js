6e0c91a3f395c826959e7d79304696706544d8ed
Raj <chanchalraj.maity@gmail.com>
2022-05-19 20:51:58 +0530

    Type_conversion.html and js file added

---
+++ js/Type_conversion.js
@@ -0,0 +1,13 @@
+//Number and date has been assigned to variable v and d 
+var v="144";
+var d= new Date('1995-2-17T03:24:00');
+
+//Conversion of string to number
+console.log("Number(v)="+Number(v));
+
+//Conversion of boolean value to number
+console.log("Number(false)"+Number(false));
+console.log("Number(true)"+Number(true));
+
+//Conversionof date to number
+console.log("Number(d)="+Number(d));
