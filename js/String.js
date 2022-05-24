22c308639ed61ee8cce176e8f566d3457fa5def9
Raj <chanchalraj.maity@gmail.com>
2022-05-24 20:15:33 +0530

    String.html and js added

---
+++ js/String.js
@@ -0,0 +1,38 @@
+//Declaration of String
+var str="My name is Raj";
+console.log(str);
+var str1="My name is Raj";
+var str2="I am an IT engineer";
+console.log(str1 +"\n"+ str2);
+
+//String length
+console.log("Length of string is: "+str.length);
+
+//String methods
+var s="Hello World";
+
+//search()
+console.log(s.search("World"));
+
+//split()
+var s1="How are you feeling today?";
+var res=s1.split(" ");
+console.log(res);
+
+//startsWith()
+console.log(s1.startsWith("How"));
+
+//toLowerCase()
+console.log(s1.toLowerCase());
+
+//toUpperCase()
+console.log(s1.toUpperCase());
+
+//charAt()
+console.log(s1.charAt(4));
+
+//indexOf()
+console.log(s1.indexOf('a'));
+
+//match()
+console.log(s1.match('match'));
