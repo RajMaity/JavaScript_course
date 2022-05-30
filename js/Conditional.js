327e1fedd08cc3fdd91b79f7d05ebebc31556612
Raj <chanchalraj.maity@gmail.com>
2022-05-30 16:07:19 +0530

    Added conditional.html and js

---
+++ js/Conditional.js
@@ -0,0 +1,19 @@
+//Table of 2
+//For loop
+for(let i=1;i<11;i++){
+	console.log("2 x "+i +"="+ 2*i);
+}
+
+//while loop
+var j=0;
+while(j<5){
+	console.log("Hello World no. :"+j);
+	j++;
+}
+
+//do while loop
+var k=0;
+do{
+	console.log("Hello World no. :"+k);
+	k++;
+}while(k<5);
