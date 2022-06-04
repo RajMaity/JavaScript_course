3e03239b3870584e7a8003374db99b73ab88cac7
Raj <chanchalraj.maity@gmail.com>
2022-06-04 17:32:22 +0530

    Object.js modified

--- js/Objects.js
+++ js/Objects.js
@@ -1,3 +1,4 @@
+//Preferred method of Object declaration in js
 let student={
 	name:"Raj Maity",
 	marks:91,
@@ -5,3 +6,13 @@ let student={
 };
 console.log("My name is: "+student.name);
 console.log("My marks in 12th was: "+student.marks);
+
+//2nd method
+let bird=new Object();
+bird.wings=2;
+bird.name="Crow";
+bird.beak=1;
+bird.fly=true;
+console.log("Can crow fly: "+bird.fly);
+console.log("Number of wings: "+bird.wings);
+console.log("Number of beak: "+bird.beak);
